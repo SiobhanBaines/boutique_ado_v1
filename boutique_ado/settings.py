865c9bea7e573f06d24ff11ff954424d4dfcf5dc
SiobhanBaines <siobhan.baines@gmail.com>
2021-05-14 19:11:16 +0000

    Crated context for shopping bag

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -69,9 +69,10 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,4 +154,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'

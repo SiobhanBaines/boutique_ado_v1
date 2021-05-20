572d3b80776677e5e4e6a28e79e1549eaede55d3
SiobhanBaines <siobhan.baines@gmail.com>
2021-05-20 14:15:04 +0000

    Add stripe card js

--- checkout/views.py
+++ checkout/views.py
@@ -14,5 +14,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51It6A8HXaEBIcMEEvxdqfecnZpIkIA4Zkgkv4KUQtfCPH3dUwUC87f8NYvWaVnQEOJc4g1riYkLVDCoA5kDcggXv00VIN5ffWh',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)

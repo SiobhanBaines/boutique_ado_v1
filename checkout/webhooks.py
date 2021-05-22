1ae8af72e122f2e89e974ce1a421cdc8b3f274d7
SiobhanBaines <siobhan.baines@gmail.com>
2021-05-22 19:33:08 +0000

    Make sure all chaanges are commited

---
+++ checkout/webhooks.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from checkout.webhook_handler import StripeWH_Handler
+
+import stripe
+
+
+@require_POST
+@csrf_exempt
+def webhook(request):
+    """Listen for webhooks from Stripe"""
+    # Setup
+    wh_secret = settings.STRIPE_WH_SECRET
+    print('wh_secret ', wh_secret)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    print('stripe.api_key ', stripe.api_key)
+    # Get the webhook data and verify its signature
+    payload = request.body
+    print('payload ', payload)
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    print('sig_header ', sig_header)
+    event = None
+
+    try:
+        event = stripe.Event.construct_from(
+            payload, sig_header, wh_secret
+            )
+        print('event ', event)
+
+    except ValueError as e:
+        # Invalid payload
+        print('valueError')
+        return HttpResponse(status=400)
+
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('stripe.error.SignatureVerificationError')
+        return HttpResponse(status=400)
+
+    except Exception as e:
+        print('Exception')
+        return HttpResponse(content=e, status=400)
+
+    print('Success')
+    return HttpResponse(status=200)
